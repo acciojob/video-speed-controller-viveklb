7e1d071d531772c0ccc13ab05663dcddada84775
vivek bharamshetti <69708672+viveklb@users.noreply.github.com>
2026-09-23 12:37:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,80 @@
-const inputs = document.querySelectorAll('.controls input');
+const player = document.querySelector(".player");
+const video = player.querySelector(".player__video");
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+const toggle = player.querySelector(".toggle");
+
+const ranges = player.querySelectorAll(".player__slider");
+
+const skipButtons = player.querySelectorAll("[data-skip]");
+
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+function updateButton() {
+  toggle.textContent = video.paused ? "►" : "❚ ❚";
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+
+  progressBar.style.flexBasis = percent + "%";
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+function skip() {
+  video.currentTime += Number(this.dataset.skip);
+}
+
+function scrub(e) {
+  const scrubTime =
+    (e.offsetX / progress.offsetWidth) * video.duration;
+
+  video.currentTime = scrubTime;
+}
+
+video.addEventListener("click", togglePlay);
+
+toggle.addEventListener("click", togglePlay);
+
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
+
+video.addEventListener("timeupdate", handleProgress);
+
+ranges.forEach(range => {
+  range.addEventListener("change", handleRangeUpdate);
+  range.addEventListener("input", handleRangeUpdate);
+});
+
+skipButtons.forEach(button => {
+  button.addEventListener("click", skip);
+});
+
+let mouseDown = false;
+
+progress.addEventListener("click", scrub);
+
+progress.addEventListener("mousemove", e => {
+  if (mouseDown) {
+    scrub(e);
+  }
+});
+
+progress.addEventListener("mousedown", () => {
+  mouseDown = true;
+});
+
+progress.addEventListener("mouseup", () => {
+  mouseDown = false;
+});
